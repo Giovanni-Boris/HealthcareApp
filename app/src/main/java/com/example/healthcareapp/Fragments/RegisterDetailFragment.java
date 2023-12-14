60246fb3b6093e07c653b74b025e4ec27d18e261
Boris Giovanni <bancasi@unsa.edu.pe>
2023-12-14 10:55:39 -0500

    using room for all queries

--- app/src/main/java/com/example/healthcareapp/Fragments/RegisterDetailFragment.java
+++ app/src/main/java/com/example/healthcareapp/Fragments/RegisterDetailFragment.java
@@ -27,12 +27,9 @@ import java.util.ArrayList;
  */
 public class RegisterDetailFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     private ArrayList<String> data;
@@ -45,15 +42,6 @@ public class RegisterDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RegisterDetailFragment newInstance(String param1, String param2) {
         RegisterDetailFragment fragment = new RegisterDetailFragment();
         Bundle args = new Bundle();
