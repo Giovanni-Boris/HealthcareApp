ca359e87aaa73288af817d0983d1999b97c97162
fdelgadov <fdelgadov@unsa.edu.pe>
2024-02-05 21:42:18 -0500

    BackGround service con interfaz de insercion
    - Ahora se pueden insertar alarmas al ROOM

--- app/src/main/java/com/example/healthcareapp/Fragments/CalendarFragment.java
+++ app/src/main/java/com/example/healthcareapp/Fragments/CalendarFragment.java
@@ -4,46 +4,38 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 
+import com.example.healthcareapp.Entity.Alarm;
 import com.example.healthcareapp.R;
+import com.example.healthcareapp.Room.Datasource;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Calendar;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
 public class CalendarFragment extends Fragment {
+    public final String TAG = "CalendarFragment";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private CalendarView calendarView;
+    private Button buttonCalendar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int dayNumber;
 
+    private Datasource datasource;
     public CalendarFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CalendarFragment newInstance(String param1, String param2) {
         CalendarFragment fragment = new CalendarFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +43,57 @@ public class CalendarFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        datasource = Datasource.newInstance(getActivity().getApplicationContext());
+
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        calendarView = view.findViewById(R.id.calendar);
+        buttonCalendar = view.findViewById(R.id.buttonCalendar);
+
+        calendarListener();
+        buttonListener();
+
+        return view;
+    }
+
+    private void calendarListener() {
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Calendar selectedDate = Calendar.getInstance();
+                selectedDate.set(year, month, dayOfMonth);
+
+                dayNumber = selectedDate.get(Calendar.DAY_OF_MONTH);
+                Log.d(TAG, "Día seleccionado: " + dayNumber);
+            }
+        });
+    }
+
+    private void buttonListener(){
+        buttonCalendar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                simpleInsert(dayNumber);
+            }
+        });
+    }
+
+    private void simpleInsert(int day) {
+        Completable.fromAction(() -> {
+                    Alarm a = new Alarm();
+                    a.setDay(day);
+                    datasource.alarmDAO().insertAlarm(a);
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> {
+                    Log.d(TAG, "Insercion realizada");
+                });
     }
 }
